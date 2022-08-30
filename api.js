aa2d3234b9d6f82426543ba00521c6c532839356
naffiziqbal <f.nafiziqbal@gmail.com>
2022-08-30 13:20:27 +0600

    adding spinner

--- api.js
+++ api.js
@@ -1,24 +1,38 @@
-// const loadPhones = async (searchText) => {
-//     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     displayPhone(data.data);
-// }
+const spinner = document.getElementById('spinner')
 
-const loadPhones = (searchText) => {
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data))
+
+const loadPhones = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhone(data.data);
 }
+
+// const loadPhones = (searchText) => {
+//     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+//         .then(res => res.json())
+//         .then(data => displayPhone(data.data))
+// }
 
 
 
 const displayPhone = phones => {
     const phoneContainer = document.getElementById("phone-container");
+    const noPhoneMessage =  document.getElementById("no-phone-message");
     phoneContainer.innerHTML = ` `;
-    phones = phones.slice(0, 10)
+    phones = phones.slice(0, 10);
+
+    // Retun Error Message If No Matching Found 
+    if(phones.length === 0){
+       noPhoneMessage.classList.remove('d-none');
+
+    }else{
+        noPhoneMessage.classList.add('d-none')
+
+    }
 
     phones.forEach(phone => {
+
         const newContainer = document.createElement('div');
         newContainer.classList.add('col')
         newContainer.innerHTML = ` 
@@ -31,18 +45,14 @@ const displayPhone = phones => {
         </div>
         `;
         phoneContainer.appendChild(newContainer);
+        spinner.classList.add('d-none')
+
     });
 
 }
-// loadPhones('');
-
-
-// document.getElementById("submit").addEventListener('click', ()=>{
-
-// })
-
 
 const searchPhone = () => {
+    spinner.classList.remove('d-none')
     const searchFiled = document.getElementById('search-filed');
     const searchText = searchFiled.value;
     loadPhones(searchText)
