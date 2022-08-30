7884f79924836b8937402dae911836822091428a
naffiziqbal <f.nafiziqbal@gmail.com>
2022-08-30 12:59:33 +0600

    A

--- api.js
+++ api.js
@@ -5,10 +5,10 @@
 //     displayPhone(data.data);
 // }
 
-const loadPhones =(searchText) =>{
-    fetch( `https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-    .then(res => res.json())
-    .then( data => displayPhone(data.data))
+const loadPhones = (searchText) => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then(res => res.json())
+        .then(data => displayPhone(data.data))
 }
 
 
@@ -16,12 +16,13 @@ const loadPhones =(searchText) =>{
 const displayPhone = phones => {
     const phoneContainer = document.getElementById("phone-container");
     phoneContainer.innerHTML = ` `;
+    phones = phones.slice(0, 10)
 
     phones.forEach(phone => {
         const newContainer = document.createElement('div');
         newContainer.classList.add('col')
         newContainer.innerHTML = ` 
-        <div class="card text-center">
+        <div class="card text-center p-5">
         <img src="${phone.image}" class="card-img-top img-fluid w-75 " alt="...">
         <div class="card-body">
         <h5 class="card-title">${phone.phone_name}</h5>
